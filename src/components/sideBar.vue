67bebbe090e05b25785f043ae403f8def3c5cd10
You <you85630@foxmail.com>
2017-07-24 15:14:58 +0800

    静态文件

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,43 +1,89 @@
 <template>
   <div class="home">
     <Menu theme="dark" active-name="1-2" :open-names="['1']">
-      <Submenu name="1">
+      <Submenu v-for="submenu in Menu" :key="submenu.index"  :name="submenu.name" >
         <template slot="title">
-          <Icon type="ios-paper"></Icon>
-          内容管理
+          <Icon :type="submenu.type"></Icon>
+          {{submenu.title}}
         </template>
-        <Menu-item name="1-1">文章管理</Menu-item>
-        <Menu-item name="1-2">评论管理</Menu-item>
-        <Menu-item name="1-3">举报管理</Menu-item>
-      </Submenu>
-      <Submenu name="2">
-        <template slot="title">
-          <Icon type="ios-people"></Icon>
-          用户管理
-        </template>
-        <Menu-item name="2-1">新增用户</Menu-item>
-        <Menu-item name="2-2">活跃用户</Menu-item>
-      </Submenu>
-      <Submenu name="3">
-        <template slot="title">
-          <Icon type="stats-bars"></Icon>
-          统计分析
-        </template>
-        <Menu-group title="使用">
-          <Menu-item name="3-1">新增和启动</Menu-item>
-          <Menu-item name="3-2">活跃分析</Menu-item>
-          <Menu-item name="3-3">时段分析</Menu-item>
-        </Menu-group>
-        <Menu-group title="留存">
-          <Menu-item name="3-4">用户留存</Menu-item>
-          <Menu-item name="3-5">流失用户</Menu-item>
-        </Menu-group>
+        <Menu-item v-for="item in submenu.item" :key="item.index" :name="item.sub">
+          <router-link :to="item.to">
+            {{item.titles}}
+          </router-link>
+        </Menu-item>
       </Submenu>
     </Menu>
   </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      Menu: [
+        {
+          name: '1',
+          type: 'ios-paper',
+          title: '内容管理',
+          item: [
+            {
+              sub: '1-1',
+              titles: '文章管理',
+              to: ''
+            }, {
+              sub: '1-2',
+              titles: '评论管理',
+              to: ''
+            }, {
+              sub: '1-3',
+              titles: '举报管理',
+              to: ''
+            }
+          ]
+        }, {
+          name: '2',
+          type: 'ios-people',
+          title: '用户管理',
+          item: [
+            {
+              sub: '2-1',
+              titles: '新增用户',
+              to: ''
+            }, {
+              sub: '2-2',
+              titles: '活跃用户',
+              to: ''
+            }
+          ]
+        }, {
+          name: '3',
+          type: 'stats-bars',
+          title: '统计分析',
+          item: [
+            {
+              sub: '3-1',
+              titles: '新增和启动',
+              to: ''
+            }, {
+              sub: '3-2',
+              titles: '活跃分析',
+              to: ''
+            }, {
+              sub: '3-3',
+              titles: '时段分析',
+              to: ''
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.ivu-menu{
+  a{
+    color: #fff;
+  }
+}
+</style>
